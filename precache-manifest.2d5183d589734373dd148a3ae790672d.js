9b08f2b16cf6d5378293b474fae60f6c863556eb
Nakshatra47 <nakshtrasharma21@gmail.com>
2022-12-24 22:18:21 +0530

    Updates

---
+++ precache-manifest.2d5183d589734373dd148a3ae790672d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "718725056d068742635f7afed99ac8ee",
+    "url": "/WheresWeather/index.html"
+  },
+  {
+    "revision": "353e61d8ecb498d7cf13",
+    "url": "/WheresWeather/static/css/main.4692f9f6.chunk.css"
+  },
+  {
+    "revision": "6448cfaae95a910cc2d2",
+    "url": "/WheresWeather/static/js/2.e07211de.chunk.js"
+  },
+  {
+    "revision": "38d5cc1cb1001b9cc38b06e2da080c18",
+    "url": "/WheresWeather/static/js/2.e07211de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "353e61d8ecb498d7cf13",
+    "url": "/WheresWeather/static/js/main.4312040e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/WheresWeather/static/js/main.4312040e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "265860f16cd99b5ad617",
+    "url": "/WheresWeather/static/js/runtime-main.8dcaa40d.js"
+  },
+  {
+    "revision": "b6265e09fa4ec9e958290feb49459a13",
+    "url": "/WheresWeather/static/media/searchicon.b6265e09.jpg"
+  },
+  {
+    "revision": "f0cd153053acebd3ad30bcd3cf0d480e",
+    "url": "/WheresWeather/static/media/weather-forecast-background.f0cd1530.jpg"
+  }
+]);
